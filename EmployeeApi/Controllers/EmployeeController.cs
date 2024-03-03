8f3b09323c7c12141b62ce43b81a4acb5f3ebdb3
zahidhasan1993 <officialzahidhasan19932021@gmail.com>
2024-03-03 12:57:25 +0600

    get all employee method added

---
+++ EmployeeApi/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+       
+        private readonly EmployeeDbContext _employeeDbContext;
+        public EmployeeController(EmployeeDbContext employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmpolyee()
+        {
+            var employees = await _employeeDbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+
+
+        }
+    }
+}
