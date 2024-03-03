7860a6e384665a746787c5fd2ae16a86a8a4ee91
zahidhasan1993 <officialzahidhasan19932021@gmail.com>
2024-03-03 17:17:46 +0600

    update,add route added

--- EmployeeApi/Controllers/EmployeeController.cs
+++ EmployeeApi/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeApi.Data;
+using EmployeeApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,6 +24,48 @@ namespace EmployeeApi.Controllers
             return Ok(employees);
 
 
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> AddEmployee ([FromBody] Employee employee)
+        {
+            await _employeeDbContext.Employees.AddAsync(employee);
+            await _employeeDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetSingleEmployee([FromRoute] int id )
+        {
+              var employee =  await _employeeDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] int id, Employee updateEmployeeReq)
+        {
+            var employee = await _employeeDbContext.Employees.FindAsync(id);
+            if (employee == null) {
+                return NotFound();
+            }
+            employee.Name = updateEmployeeReq.Name;
+            employee.Email = updateEmployeeReq.Email;
+            employee.Address = updateEmployeeReq.Address;   
+            employee.Salary = updateEmployeeReq.Salary;
+
+            await _employeeDbContext.SaveChangesAsync();
+
+            return Ok(employee);
         }
     }
 }
